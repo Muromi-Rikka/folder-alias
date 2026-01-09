6e829d19e070914c0557666e5b3536b8684e219f
skyfeiz <webzhangfei@163.com>
2026-01-09 11:53:56 +0800

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,12 @@ const { activate, deactivate } = defineExtension(async () => {
   for (let index = 0; index < workspace.workspaceFolders.length; index++) {
     const ws = workspace.workspaceFolders[index];
     const workspaceDir: string = ws.uri.fsPath;
-    
+
     const vscodeConfigPath = joinURL(workspaceDir, ".vscode", "folder-alias.json");
     const vscodePrivateConfigPath = joinURL(workspaceDir, ".vscode", "private-folder-alias.json");
     const rootConfigPath = joinURL(workspaceDir, "folder-alias.json");
     const rootPrivateConfigPath = joinURL(workspaceDir, "private-folder-alias.json");
-    
+
     // 只在根目录创建配置文件，但如果.vscode目录下已存在配置文件，则不创建
     if (!fs.existsSync(vscodeConfigPath) && !fs.existsSync(rootConfigPath)) {
       writeConfig(rootConfigPath, {});
